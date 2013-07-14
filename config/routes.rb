8926135dc70a874d8b114a40afbdc14ad77e5b54
Matt Coffman <mattcoff@gmail.com>
2013-07-14 09:00:19 -0500

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Omr::Application.routes.draw do
   resources :pins
 
-
   devise_for :users
 
-  root :to => "pages#home"
+  root :to => "pins#index"
   get 'about' => 'pages#about'
   # The priority is based upon order of creation:
   # first created -> highest priority.
